5a30e7e5725b7b8a080c981709fd9f95ee7cb94c
abraham martinez <abrahammartinez@abrahams-MacBook-Pro.local>
2018-07-17 16:07:45 -0700

    debugAfternoon

--- stage-4/src/redux/store.js
+++ stage-4/src/redux/store.js
@@ -1,5 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import reducer from './reducer';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
+
+
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
